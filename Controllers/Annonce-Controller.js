a4e92f4bf20e2144a6d17e2be6b46dc565b8cc0c
maroua <maroua2012@gmail.com>
2023-01-10 10:51:11 +0100

    optimisation

--- Controllers/Annonce-Controller.js
+++ Controllers/Annonce-Controller.js
@@ -3,8 +3,8 @@ const bcrypt = require("bcryptjs");
 const { validationResult } = require("express-validator");
 const Annonce = require("../Moduls/Annonce");
 const User = require("../Moduls/User");
-const { default: mongoose } = require("mongoose");
 
+/**POST creer une annonce */
 exports.createAnnonce = async (req, res, next) => {
 	/**recuperer les informations d'une annonce */
 	const { nom, prix, description, qteDispo, categorie } = req.body;
@@ -20,27 +20,22 @@ exports.createAnnonce = async (req, res, next) => {
 
 			return res.status(422).json({ errors: errorsClient });
 		}
-		/**creer une instance Annonce */
 
-		const annonce = new Annonce({ nom: nom, prix: prix, description: description, qteDispo: qteDispo, categorie: categorie, user: req.userData.id });
-		console.log(annonce);
 		/**verifier si l'utilisateur existe bien dans la base de donnees */
 		user = await User.findById(req.userData.id);
 		if (!user) {
 			return res.status(404).json({ message: "utilisateur n'existe pas" });
 		}
-		/**utiliser les transaction et les sessions */
-		const session = await mongoose.startSession();
-		session.startTransaction();
-		result = await annonce.save({ session });
-		//faire un commit
-		await session.commitTransaction();
+
+		result = await Annonce.create({ nom, prix, description, qteDispo, categorie, user: req.userData.id });
+
 		return res.status(201).json({ message: "Annonce crée", result: result });
 	} catch (error) {
 		console.log(error);
 		res.status(500).json({ error: error });
 	}
 };
+/**GET get all annonces */
 exports.getAnnonces = async (req, res, next) => {
 	try {
 		const annonces = await Annonce.find();
